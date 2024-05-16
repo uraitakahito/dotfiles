4046f795bdc86ada16994e49bf885d76ea861420
Takahito Urai <uraitakahito@gmail.com>
2024-05-16 23:50:04 +0900

    Add notes about the has-vscode-remote-containers function.

--- zsh/modules/helper/init.sh
+++ zsh/modules/helper/init.sh
@@ -8,7 +8,10 @@ function is-vscode {
   [[ "$TERM_PROGRAM" == "vscode" ]]
 }
 
-# Checks if running on VSCode and Remote Containers.
+# Checks if script is being executed by VS Code.
+# NOTE:
+#  1. After installation, this environment variable will be removed.
+#  2. When install.sh is executed, the 'code' CLI is not yet installed.
 function has-vscode-remote-containers {
   [[ -n "${VSCODE_REMOTE_CONTAINERS_SESSION:-}" ]]
 }
