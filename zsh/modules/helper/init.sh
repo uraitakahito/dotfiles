7c08cc8624bdb08e9e6fe9ea4ff6dfc4dca1e502
Takahito Urai <uraitakahito@gmail.com>
2024-05-19 10:37:43 +0900

    Add logging functions.

--- zsh/modules/helper/init.sh
+++ zsh/modules/helper/init.sh
@@ -1,3 +1,24 @@
+#
+# logging functions
+#
+my_log() {
+	local type="$1"; shift
+	# accept argument string or stdin
+	local text="$*"; if [ "$#" -eq 0 ]; then text="$(cat)"; fi
+	local dt; dt="$(date -R)"
+	printf '%s [%s] [Entrypoint]: %s\n' "$dt" "$type" "$text"
+}
+my_note() {
+	my_log Note "$@"
+}
+my_warn() {
+	my_log Warn "$@" >&2
+}
+my_error() {
+	my_log ERROR "$@" >&2
+	exit 1
+}
+
 # Checks if running on MacOS Darwin.
 function is-darwin {
   [[ "$OSTYPE" == darwin* ]]
